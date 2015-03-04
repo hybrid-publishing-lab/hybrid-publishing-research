59ae0b215cc47eb3eff4d89c76ccb58e39fa9fc7
Johannes Amorosa <amorosa@inkubator.leuphana.de>
2015-03-04 16:32:51 +0100

    integrated bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,146 +1,160 @@
 /* global module:false */
 module.exports = function(grunt) {
 
-	var port = grunt.option('port') || 8000;
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * consortium-viewer.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * https://github.com/consortium/hybrid-publishing-research\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2015 Hybrid Publishing COnsortium\n' +
-				' */'
-		},
+    var port = grunt.option('port') || 8000;
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner:
+                '/*!\n' +
+                ' * consortium-viewer.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+                ' * https://github.com/consortium/hybrid-publishing-research\n' +
+                ' * MIT licensed\n' +
+                ' *\n' +
+                ' * Copyright (C) 2015 Hybrid Publishing COnsortium\n' +
+                ' */'
+        },
 
-		qunit: {
-			files: [ 'test/*.html' ]
-		},
-		
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>\n'
-			},
-			build: {
-				src: 'js/consortium-viewer.js',
-				dest: 'js/consortium-viewer.min.js'
-			}
-		},
+        qunit: {
+            files: [ 'test/*.html' ]
+        },
+        
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>\n'
+            },
+            build: {
+                src: 'js/consortium-viewer.js',
+                dest: 'js/consortium-viewer.min.js'
+            }
+        },
 
-		jshint: {
-			options: {
-				curly: false,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				eqnull: true,
-				browser: true,
-				expr: true,
-				globals: {
-					head: false,
-					module: false,
-					console: false,
-					unescape: false,
-					define: false,
-					exports: false
-				}
-			},
-			files: [ 'Gruntfile.js', 'js/consortium-viewer.js' ]
-		},
+        jshint: {
+            options: {
+                curly: false,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                eqnull: true,
+                browser: true,
+                expr: true,
+                globals: {
+                    head: false,
+                    module: false,
+                    console: false,
+                    unescape: false,
+                    define: false,
+                    exports: false
+                }
+            },
+            files: [ 'Gruntfile.js', 'js/consortium-viewer.js' ]
+        },
 
-		connect: {
-			server: {
-				options: {
-					port: port,
-					base: '.',
-					livereload: true,
-					open: true
-				}
-			}
-		},
-		
-		html_sitemap: {
-			options: {
-				searchPath: 'docs/',
-				template: 'template.html'
-			},
-			files: {
-				'index.html': ['docs/**/*.html']
-			}
-		},
+        connect: {
+            server: {
+                options: {
+                    port: port,
+                    base: '.',
+                    livereload: true,
+                    open: true
+                }
+            }
+        },
+        
+        html_sitemap: {
+            options: {
+                searchPath: 'docs/',
+                template: 'template.html'
+            },
+            files: {
+                'index.html': ['docs/**/*.html']
+            }
+        },
 
-		fileindex: {
-			list: {
-				options: {
-					format: 'json_flat',
-					pretty: true
-				},
-				files: [
-					{dest: 'list.json', src: ['docs/**/*.html']}
-				]
-			},
-		
-		},
-		
-		watch: {
-			options: {
-				livereload: true
-			},
-			js: {
-				files: [ 'Gruntfile.js', 'js/consortium-viewer.js' ],
-				tasks: 'js'
-			},
-			fileindex: {
-				files: [ 'docs/**/*.html' ],
-				tasks: 'fileindex'
-			},
-			html_sitemap: {
-				files: [ 'docs/**/*.html' ],
-				options: {
-					searchPath: 'docs/',
-					template: 'template.html'
-				}
-			},
-			html: {
-				files: [ 'index.html']
-			}
-		}
+        fileindex: {
+            list: {
+                options: {
+                    format: 'json_flat',
+                    pretty: true
+                },
+                files: [
+                    {dest: 'list.json', src: ['docs/**/*.html']}
+                ]
+            },
+        
+        },
+        
+        watch: {
+            options: {
+                livereload: true
+            },
+            js: {
+                files: [ 'Gruntfile.js', 'js/consortium-viewer.js' ],
+                tasks: 'js'
+            },
+            fileindex: {
+                files: [ 'docs/**/*.html' ],
+                tasks: 'fileindex'
+            },
+            html_sitemap: {
+                files: [ 'docs/**/*.html' ],
+                options: {
+                    searchPath: 'docs/',
+                    template: 'template.html'
+                }
+            },
+            html: {
+                files: [ 'index.html']
+            }
+        },
 
-	});
+        bower: {
+            dev: {
+                dest: 'lib/',
+                js_dest: 'lib/js/',
+                css_dest: 'lib/css/',
+            fonts_dest: 'lib/font/',  
+            options: {
+                expand: true
+        }
+  }
+}
 
-	grunt.event.on('watch', function(action, filepath, target) {
-		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
-	});
+    });
+
+    grunt.event.on('watch', function(action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
 
 
-	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    // Dependencies
+    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-html-sitemap');
-	grunt.loadNpmTasks( 'grunt-fileindex' );
-	// Default task
-	grunt.registerTask( 'default', [ 'js' ] );
-	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+    grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks( 'grunt-autoprefixer' );
+    grunt.loadNpmTasks( 'grunt-html-sitemap');
+    grunt.loadNpmTasks( 'grunt-fileindex' );
+    grunt.loadNpmTasks( 'grunt-bower' );
 
-	// Serve presentation locally
-	grunt.registerTask( 'serve', [ 'fileindex', 'html_sitemap', 'connect', 'watch'] );
-	grunt.registerTask( 'index', [ 'fileindex', 'html_sitemap' ] );
+    // Default task
+    grunt.registerTask( 'default', [ 'js' ] );
+    // JS task
+    grunt.registerTask( 'js', [ 'bower', 'jshint', 'uglify', 'qunit' ] );
 
-	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
+    // Serve presentation locally
+    grunt.registerTask( 'serve', [ 'fileindex', 'html_sitemap', 'connect', 'watch'] );
+    grunt.registerTask( 'index', [ 'fileindex', 'html_sitemap' ] );
+
+    // Run tests
+    grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
 
 };
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
+** IN PROGRESS **
+
 About
 -----
-
-
-
+This repository is the consortium.io document celloction. Its complied in a redistributable way.
+We're hosting this on docs.consortium.io. If you just wan't to download the papers use the
+folder stable/
 
 Installation
 ------------
@@ -31,4 +33,45 @@ if this doesn't fail use
 
 Your Browser will open and you can visit the sites on [http://localhost:8000](http://localhost:8000)
 
+Edit or add Content
+-------------------
+This repository comes with a node.js setup to serve the documents locally.
 
+1. Install [https://www.npmjs.com/](npm) via your package manager.
+2. Clone the repository
+	
+	 git clone https://github.com/consortium/hybrid-publishing-research.git
+
+2. Open a terminal and change directory to hybrid-publishing-research
+
+	  cd hybrid-publishing-research
+
+3. Install npm dependancies
+
+4. Use grunt to handle all tasks
+
+	grunt 
+
+5. Create a git branch 
+
+	git checkout mybranch
+
+6. Edit the files with the your favorite text editor. (We recommend sublimetext).
+You can copy the docs/_template folder as a starter for your new dossier.
+
+7. TODO *Once your done you can generate new stable pdfs with
+
+	grunt createstablepdf
+
+8. Add, Commit and push your files to the Branch
+
+9. Create a new pull request
+
+
+
+
+	
+
+
+Licence
+-------
