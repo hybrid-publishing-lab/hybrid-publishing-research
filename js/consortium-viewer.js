a3199d1c199a8678d29ac633dcf619038fbd7425
Johannes Amorosa <amorosa@inkubator.leuphana.de>
2015-03-17 17:17:14 +0100

    delete false div, add dependencies, updated gitignore, code improvement

--- js/consortium-viewer.js
+++ js/consortium-viewer.js
@@ -1,26 +1,27 @@
 var App = angular.module('App', []);
 
-App.controller('MainCtrl', function($scope, $http) {
+App.controller('SpaceCtrl', function($scope, $http) {
     $http.get('list.json')
         .then(function(res){
             $scope.list = res.data;
         });
     
-    $http.get('validation-report.json')
+    $http.get('validation-status.json')
         .then(function(res){
-            $scope.validationreport = res.data;
+            $scope.validationstatus = res.data;
         });
 
-        $http.get('validation-status.json')
+    $http.get('index.json')
         .then(function(res){
             /*{
-        	        "docs/book_liberation_manifesto/Book_Liberation_Manifesto.html":false,
+                "docs/book_liberation_manifesto/Book_Liberation_Manifesto.html":false,
                 "docs/empowering_publishers_to_innovate/Empowering_Publishers_to_Innovate.html":true
             }*/
-            $scope.validationstatus = res.data;
+            $scope.indexlist = res.data;
 
         });
 
+    // Just display failed documents 
     $scope.filterValid = function(items) {
         var result = {};
         angular.forEach(items, function(value, key) {
